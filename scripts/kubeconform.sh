bb05ff66d9c7b552de8e13edf459b2e5e8815c70
Tanguille <Tanguille@hotmail.be>
2024-10-20 19:55:20 +0200

    fix(ci): skip template values

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -17,36 +17,35 @@ kubeconform_args=(
     "default"
     "-schema-location"
     "https://kubernetes-schemas.pages.dev/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
+    "-skip-kinds"
+    "ReplicationSource,ReplicationDestination"
     "-verbose"
 )
 
 echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
-find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
+find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
     kubeconform "${kubeconform_args[@]}" "${file}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
+        exit 1
     fi
 done
 
 echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
-find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
+find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+    echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
+    kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
+        kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
+        exit 1
     fi
 done
 
 echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
-find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
+find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+    echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
+    kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
+        kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
+        exit 1
     fi
 done
